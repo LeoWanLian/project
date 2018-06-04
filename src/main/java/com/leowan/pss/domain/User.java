076315379e3b9f522b4e5d5142521d58dbb128cf
leowan <20972647@qq.com>
2018-06-04 22:42:36 +0800

    demo·name age

--- src/main/java/com/leowan/pss/domain/User.java
+++ src/main/java/com/leowan/pss/domain/User.java
@@ -4,5 +4,19 @@ public class User {
     private String name;
     private Long age;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
 }
