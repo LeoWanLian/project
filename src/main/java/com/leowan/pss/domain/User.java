4274fd471e5504e89939b8ab02ebe283a14d6900
leowan <20972647@qq.com>
2018-06-14 20:41:12 +0800

    shanchu get/set

--- src/main/java/com/leowan/pss/domain/User.java
+++ src/main/java/com/leowan/pss/domain/User.java
@@ -4,23 +4,7 @@ public class User {
     private String name;
     private Long age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAge() {
-        return age;
-    }
-
-    public void setAge(Long age) {
-
-        this.age = age;
-    }
-
+    
     public void add(){
         System.out.println("leowan");
     }
